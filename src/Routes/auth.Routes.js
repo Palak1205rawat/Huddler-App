02dc6bad7cc8eabee51e2a3f1d38ac06bb216ae2
Palak1205rawat <youarewillfull12345@gmail.com>
2026-04-23 01:23:17 +0530

    recreate auth routes

---
+++ src/Routes/auth.Routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const user_controller = require("../Controllers/User.Controllers.js");
+
+router.get("/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get("/google/callback",
+  passport.authenticate("google", { failureRedirect: "/user/login" }),
+  user_controller.Post_google
+);
+
+module.exports = router;
